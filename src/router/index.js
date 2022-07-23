be444a32031cb6804616dd4fe42e01a0bd9a0b14
严家辉 <yanjiahui@192.168.1.9>
2022-07-23 23:28:15 +0800

    fix: fixbug

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import routes from './routes'
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHistory(),
     routes,
     scrollBehavior: () => ({ left: 0, top: 0 })
 })
