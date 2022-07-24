d660e9874221abac02cda7c28a8130501ce6749d
严家辉 <yanjiahui@192.168.1.9>
2022-07-24 23:37:10 +0800

    feat: 拆分组件

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -10,15 +10,7 @@
           <IconCaretRight v-if="collapsed" />
           <IconCaretLeft v-else />
         </a-button>
-        <div class="actions_dropdown">
-          <a-dropdown @select="handleSelect">
-            <a-button> <icon-user />严家辉 </a-button>
-            <template #content>
-              <a-doption>个人信息</a-doption>
-              <a-doption>退出登录</a-doption>
-            </template>
-          </a-dropdown>
-        </div>
+        <LayoutUser />
       </a-layout-header>
       <a-layout style="padding: 0 24px">
         <a-breadcrumb :style="{ margin: '16px 0' }">
@@ -40,7 +32,8 @@
 import { defineComponent, ref, watch } from "vue";
 import { Message } from "@arco-design/web-vue";
 import NavMenu from "@/components/nav-menu.vue";
-import { useRoute, useRouter } from "vue-router";
+import LayoutUser from "@/components/layout-user.vue";
+import { useRoute } from "vue-router";
 import { IconCaretRight, IconCaretLeft } from "@arco-design/web-vue/es/icon";
 
 export default defineComponent({
@@ -49,12 +42,12 @@ export default defineComponent({
     NavMenu,
     IconCaretRight,
     IconCaretLeft,
+    LayoutUser
   },
   setup() {
     const route = useRoute();
     const collapsed = ref(false);
     const hidden = ref(false);
-    const $router = useRouter();
     watch(
       () => route.meta,
       (val) => (hidden.value = val.hidden)
@@ -64,14 +57,11 @@ export default defineComponent({
       collapsed.value = !collapsed.value;
     };
 
-    const handleSelect = () => {
-      $router.push("/login");
-    };
+
     return {
       collapsed,
       onCollapse,
       hidden,
-      handleSelect,
       onClickMenuItem(key) {
         Message.info({ content: `You select ${key}`, showIcon: true });
       },
@@ -129,10 +119,5 @@ export default defineComponent({
   display: flex;
   align-items: center;
 }
-.actions_dropdown {
-  flex: 1;
-  display: flex;
-  justify-content: flex-end;
-  padding-right: 20px;
-}
+
 </style>
