ecf3f85bd38714978de4fd27533505fcf9447d26
严家辉 <yanjiahui@192.168.0.107>
2022-08-23 21:00:45 +0800

    feat: 新增保存系统配置功能(#1)

--- src/apis/system.js
+++ src/apis/system.js
@@ -3,5 +3,8 @@ import request from '@/utils/request';
 export default {
     getSystemList() {
         return request.get(`/system/list`, {  })
+    },
+    updateSystem(data) {
+        return request.patch(`/system/update`, data)
     }
 }
--- src/views/system/index.vue
+++ src/views/system/index.vue
@@ -5,16 +5,18 @@
         <Notification />
       </a-tab-pane>
       <a-tab-pane key="2" title="用户设置"> Content of Tab Panel 2 </a-tab-pane>
-      <a-tab-pane key="3" title="主题安装"> Content of Tab Panel 2 </a-tab-pane>
+      <a-tab-pane key="3" title="主题安装"> <Theme /> </a-tab-pane>
     </a-tabs>
   </div>
 </template>
 <script>
-import Notification from "./components/notification"
+import Notification from "./components/notification";
+import Theme from "./components/theme";
 export default {
   name: "system-config",
   components: {
-    Notification
+    Notification,
+    Theme
   },
   setup() {
     // return {};
