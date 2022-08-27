870da0ab65b0690e2ebe10f567ec03ffdb13fc4e
严家辉 <yanjiahui@192.168.0.101>
2022-08-27 09:58:00 +0800

    feat: 新增站点构建日志

--- src/apis/log.js
+++ src/apis/log.js
@@ -3,5 +3,11 @@ import request from '@/utils/request';
 export default {
     getLogList(params = {}) {
         return request.get(`/log/list`, { params })
+    },
+    getBuildLogList(siteId, params = {}) {
+        return request.get(`/log/build/${siteId}/list`, { params })
+    },
+    getBuildLogInfo(buildId) {
+        return request.get(`/log/build/${buildId}/info`)
     }
 }
